1bc907df20f4704ab180d032c5db3e8a6380559a
Paige <paigephilippart@gmail.com>
2025-08-26 17:46:43 -0500

    initial commit

---
+++ src/main/java/com/kitchen/helper/KitchenHelperApplication.java
@@ -0,0 +1,13 @@
+package com.kitchen.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenHelperApplication.class, args);
+	}
+
+}
